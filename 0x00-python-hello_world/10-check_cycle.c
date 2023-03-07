51a3e178bc92c1b0695b763f20d32c7f4c08a05e
nyaberi <mayakanyaberi@gmail.com>
2023-03-07 03:02:08 +0300

    floyd cycle detection algorithm

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: pointer to the list node of a linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow_p = list, *fast_p = list;
+
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = fast_p->next->next;
+
+		if (slow_p == fast_p)
+			return (1);
+	}
+	return (0);
+}
