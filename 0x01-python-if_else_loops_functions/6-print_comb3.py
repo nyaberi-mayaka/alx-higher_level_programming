27e1597be52c4798e27385ba7ad604360237f67d
nyaberi <mayakanyaberi@gmail.com>
2023-03-07 23:28:15 +0300

    -print_comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for i in range(9):
     for j in range(10):
         if i < j:
-                print("{:d}{:d}".format(i % 10, j % 10), end="")
-                if i == 8 and j == 9:
-                    print()
-                    break
-                print(", ", end="")
+            print("{:d}{:d}".format(i % 10, j % 10), end="")
+            if i == 8 and j == 9:
+                print()
+                break
+            print(", ", end="")
