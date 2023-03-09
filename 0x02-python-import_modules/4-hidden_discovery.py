4d34c94f038be39b34341b84515406d2991ed7aa
nyaberi <mayakanyaberi@gmail.com>
2023-03-09 22:51:50 +0300

    4-hidden_discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    if __name__ == "__main__":
+        import hidden_4
+
+        for strs in dir(hidden_4):
+            if strs[:2] != "__":
+                print(strs)
