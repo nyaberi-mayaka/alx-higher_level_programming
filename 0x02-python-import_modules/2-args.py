0a88ea31e2871ae8314456e44a18108b53f6c9fb
nyaberi <mayakanyaberi@gmail.com>
2023-03-09 15:34:19 +0300

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def main():
+    import sys
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{0:d} {1:s}".format(i, "arguments."))
+    elif i > 0:
+        print("{0:d} {1:s}{2:s}".format(i, "argument", ":" if i == 1 else "s:"))
+
+    for i, strs in enumerate(sys.argv):
+        if i == 0:
+            continue
+        print("{0:d}: {1:s}".format(i, strs))
+
+
+if __name__ == "__main__":
+    main()
