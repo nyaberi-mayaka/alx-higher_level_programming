984847da559d2d2ba22b2a7dec348fad1800b9f5
nyaberi <mayakanyaberi@gmail.com>
2023-03-09 15:57:21 +0300

    3-infinite_add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def main():
+    import sys
+
+    if len(sys.argv) == 1:
+        print("{:d}".format(0))
+
+    else:
+        result = 0
+
+        for i ,strs in enumerate(sys.argv):
+            if i == 0:
+                continue
+
+            result += int(strs)
+        print("{:d}".format(result))
+
+
+if __name__ == "__main__":
+    main()
