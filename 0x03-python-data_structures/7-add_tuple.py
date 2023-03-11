54f691b0fad80dce5fbe10bd128f5eb42b7a508e
nyaberi <mayakanyaberi@gmail.com>
2023-03-11 06:04:14 +0300

    7-add_tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    lst = [[] for i in range(2)]
+    for i in range(2):
+        if len(tuple_a) < i + 1:
+            x = 0
+        else:
+            x = tuple_a[i]
+
+        if len(tuple_b) < i + 1:
+            y = 0
+        else:
+            y = tuple_b[i]
+
+        lst[i] = x + y
+    tuple(lst)
+    return (lst)
