d665cb63910df2ab094545c2a6d44aa7fa398120
nyaberi <mayakanyaberi@gmail.com>
2023-03-12 04:01:49 +0300

    8-multiple_returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return ((len(sentence), sentence[0]) if len(sentence) > 0 else None)
+    tuple_a = ((len(sentence), sentence[0]) if sentence != "" else "None")
+    return (tuple_a)
