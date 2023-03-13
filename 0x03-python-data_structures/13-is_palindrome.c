f481d286fb54091e13702b2d5db9c7113ea9b0c2
Mayaka Nyaberi <47862028+nyaberi-mayaka@users.noreply.github.com>
2023-03-13 23:33:10 +0300

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,112 +1,73 @@
 #include "lists.h"
 
-void push(list_t **, listint_t *);
-int pop(list_t **);
-void frees(list_t *head);
+/**
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ * Return: pointer to the first node in the new list
+ */
+
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome.
- *
- * @head: the first node of the list
- *
- * Return:  0 if it is not a palindrome, 1 if it is a palindrome
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: head
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
+
 int is_palindrome(listint_t **head)
 {
-	list_t *front = NULL;
-	listint_t *slow, *fast, *curr, *p1;
-	int flag = 0;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	if (head == NULL || (*head)->next == NULL)
-	{
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	}
-/*Find the middle of the list using the "slow and fast" pointer technique*/
-	slow = fast = *head;
-	while (fast != NULL && fast->next != NULL)
-		slow = slow->next, fast = fast->next->next;
 
-/*create a stack*/
-	curr = slow;
-	while (curr != NULL)
+	while (1)
 	{
-		push(&front, curr);
-		curr = curr->next;
-	}
-/*check if the first half of the list match and the stack match*/
-	p1 = *head;
-	while (front)
-	{
-		if (p1->n == pop(&front))
-			p1 = p1->next;
-
-		else
+		fast = fast->next->next;
+		if (!fast)
 		{
-			flag = 1;
-			frees(front);
+			dup = slow->next;
 			break;
 		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-	return (flag == 0 ? 1 : 0);
-}
 
+	reverse_listint(&dup);
 
-/**
- * push - adds a new element to the top of stack
- * @temp: the data to store on top of the stack
- * @head: the address of the top most element of the stack
- */
-
-void push(list_t **head, listint_t *temp)
-{
-	list_t *newnode = malloc(sizeof(list_t));
-
-	if (newnode == NULL)
+	while (dup && temp)
 	{
-		printf("Unable to allocate memory\n");
-		return;
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
 	}
 
-	newnode->data = temp;
-	newnode->next = *head;
-	*head = newnode;
-}
+	if (!dup)
+		return (1);
 
-/**
- * pop - removes the top most element of the stack
- * @head: location of the top most element of the stack
- * Return: the value of the intger stored by the linked list node of
- * the top most element of the stack
- */
-
-int pop(list_t **head)
-{
-	int x;
-	list_t *temp;
-
-	temp = *head;
-	*head = temp->next;
-	x = temp->data->n;
-	free(temp);
-
-	return (x);
-}
-
-/**
- * frees - frees a stack data structure
- * @head: pointer to the first node of the structure
- */
-
-void frees(list_t *head)
-{
-	list_t *temp = head;
-
-	while (temp)
-	{
-		temp = temp->next;
-
-		/*free(head->data);*/
-		free(head);
-		head = temp;
-	}
+	return (0);
 }
