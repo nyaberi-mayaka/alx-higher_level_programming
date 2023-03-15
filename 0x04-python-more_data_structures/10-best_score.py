76d0ce3f137bf635aca985d4ce9c9697176f220e
nyaberi <mayakanyaberi@gmail.com>
2023-03-15 23:26:46 +0300

    10-best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict):
+    if a_dictionary:
         x = list(a_dictionary.values())[0]
+        y = ""
         for i, v in a_dictionary.items():
-            if v > x:
-                x, y = v, i
-        return (y)
-    else:
-        return (None)
+            if v >= x:
+                x = v
+                y = i
+    return (y)
