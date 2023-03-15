7ea3041b69d79f577a206b0e064ba1ae1230b6a6
nyaberi <mayakanyaberi@gmail.com>
2023-03-15 19:03:22 +0300

    0-square_matrix_simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix1 = [[j * j for j in i] for i in matrix]
+    # matrix1 = list(map(lambda i : list(map(lambda j : j**2, i)), matrix))
+    return (matrix1)
