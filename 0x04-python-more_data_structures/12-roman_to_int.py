caaff422819d2f0fdfc528c67c13128241b96c4d
nyaberi <mayakanyaberi@gmail.com>
2023-03-16 00:42:38 +0300

    12-roman_to_int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string:
+        # check if roman_string is a string wilt len less than 9
+        if isinstance(roman_string, str) and len(roman_string) <= 9:
+            integer = 0
+            roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                     'C': 100, 'D': 500, 'M': 1000}
+            for i in range(len(roman_string)):
+                if i > 0 and roman[roman_string[i]] >\
+                   roman[roman_string[i - 1]]:
+                    integer += roman[roman_string[i]] -\
+                        2 * roman[roman_string[i - 1]]
+                else:
+                    integer += roman[roman_string[i]]
+            return (integer)
