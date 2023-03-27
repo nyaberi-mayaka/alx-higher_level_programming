b0c3a29e9dd75bae2118855ee30c42793fab4408
nyaberi <mayakanyaberi@gmail.com>
2023-03-27 22:19:58 +0300

    0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,10 +7,11 @@ def safe_print_list(my_list=[], x=0):
             if j < x:
                 print("{}".format(i), end="")
                 j += 1
-            else:
-                raise IndexExceeds
 
-    except IndexExceeds:
+                """            else:
+                raise IndexExceeds
+                """
+    except Exception as e:
         print()
     else:
         print()
@@ -18,5 +19,7 @@ def safe_print_list(my_list=[], x=0):
     return (j)
 
 
+""""
 class IndexExceeds(Exception):
     pass
+"""
