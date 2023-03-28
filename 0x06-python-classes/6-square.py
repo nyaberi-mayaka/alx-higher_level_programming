09e5a5b68987847b3b1ff6edd6389966e74abccc
nyaberi <mayakanyaberi@gmail.com>
2023-03-29 02:04:19 +0300

    6-square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,7 +34,9 @@ class Square:
     def position(self, value):
         """position setter"""
 
-        if isinstance(value, tuple) or len(value) == 2:
+        if isinstance(value, tuple) or len(value) == 2 or \
+           not isinstance(value[0], int) or not isinstance(value[1], int)\
+           or value[0] < 0 or value[1] < 0:
             self.__position = value
 
         else:
