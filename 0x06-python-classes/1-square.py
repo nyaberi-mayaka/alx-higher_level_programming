6da4d2b95c7718254dc3c8b5b5d9015a9697f5e3
nyaberi <mayakanyaberi@gmail.com>
2023-03-28 23:06:43 +0300

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+ A class Square that defines a square
+"""
+
+
+class Square:
+    """
+    class square reperesenting a square object
+    """
+    def __init__(self, size):
+        self.__size = size
