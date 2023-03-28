e96bcab6e05cff827c4a38a67e430c368f84b1e9
nyaberi <mayakanyaberi@gmail.com>
2023-03-28 23:41:49 +0300

    2-square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""
+ A class Square that defines a square
+"""
+
+
+class Square:
+    """
+    class square reperesenting a square object
+    """
+
+
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
