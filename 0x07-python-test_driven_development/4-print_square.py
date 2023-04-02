a1cee927130c4b7606edf265d7875a1d2f9730ac
nyaberi <mayakanyaberi@gmail.com>
2023-04-03 00:55:47 +0300

    added check for 0 arguments

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-def print_square(size):
+def print_square(size=0):
     """Prints a square with the character #
-    Checks for logical and Type errors and raises the corresponding Exceptions
+    Checks for logical and Type errors and raises the corresponding
+    Exceptions
 
     Args:
         size (int): The length of the square
@@ -10,7 +11,12 @@ def print_square(size):
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
+
+    if size != size or size == float('inf'):
+        size = 1
+
     for i in range(size):
         print(size * "#")
