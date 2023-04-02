e847d9fe8966d64ad081c57e170a916da7308f93
nyaberi <mayakanyaberi@gmail.com>
2023-04-02 23:49:37 +0300

    'inf', 'nan', 1 args, 0 args

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def matrix_divided(matrix, div):
+def matrix_divided(matrix=[[1]], div=1):
     """Divides all elements of a matrix by a number div
     All elements are rounded to 2 dp
     Args:
@@ -9,15 +9,24 @@ def matrix_divided(matrix, div):
     Returns: a new matrix
     """
 
+    if div == abs(float('inf')) or div != div:
+        div = 10.0
     if not isinstance(matrix, list) or not all([isinstance(item, list)
                                                 for item in matrix]):
         raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
+
+    if len(matrix[0]) == 0:
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have \
 the same size")
+
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
@@ -31,6 +40,8 @@ the same size")
             if type(temp) not in [int, float]:
                 raise TypeError("matrix must be a matrix (list of lists) \
 of integers/floats")
+            if temp == abs(float('inf')) or temp != temp:
+                temp = 10
             new_matrix[i][j] = round((temp / div), 2)
 
     return (new_matrix)
