f97c5ab6043d2c8ee7578bd4fe9520ca5cd285c5
nyaberi <mayakanyaberi@gmail.com>
2023-04-05 05:10:51 +0300

    Indentation errors

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,8 +5,8 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
      """Return the multiplication of two matrices.
-    Args:
+     Args:
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
-    """
-    return (np.matmul(m_a, m_b))
+     """
+     return (np.matmul(m_a, m_b))
