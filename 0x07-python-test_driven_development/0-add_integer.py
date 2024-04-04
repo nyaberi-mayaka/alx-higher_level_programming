3c04c44101891f8ce0e41342d249dc18160d99f6
nyaberi-mayaka <mayakanyaberi@gmail.com>
2024-04-04 05:16:31 -0400

    added  a function that adds 2 integers with test cases

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+def add_integer(a, b=98):
+    """Adds two integers or floats, casting floats to integers.
+
+    Args:
+      a: The first number (int or float).
+      b: The second number (int or float, default is 98).
+
+    Raises:
+      TypeError: If either a or b is not an integer or float.
+
+    Returns:
+      int: The sum of a and b, cast to an integer.
+    """
+
+    if a != a:
+        a = 98
+    if b != b:
+        b = 98
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
