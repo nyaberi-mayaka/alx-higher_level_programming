06fc2c7d227984cff2461c7b63a13d5b69c87f6d
nyaberi-mayaka <mayakanyaberi@gmail.com>
2024-04-06 04:30:39 -0400

    a function that prints a square with the character # with tet cases

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """Prints a square of the specified size using the # character.
+
+    Args:
+      size: The size of the square (must be a non-negative integer).
+
+    Raises:
+      TypeError: If size is not an integer.
+      ValueError: If size is negative.
+    """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for row in range(size):
+            for col in range(size):
+                print("#", end="")
+            print("")
