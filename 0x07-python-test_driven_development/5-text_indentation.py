539fa7114d12eeccd297192346376466b6278c2c
nyaberi-mayaka <mayakanyaberi@gmail.com>
2024-04-06 06:06:54 -0400

    a function that prints a text with 2 new lines after each of these characters: ., ? and : and test cases

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+def text_indentation(text):
+    """Prints a text with two new lines after ., ?, and : characters.
+
+    Args:
+     text: A string representing the text to be indented.
+
+    Raises:
+     TypeError: If text is not a string.
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    words = text.split()
+
+    for i, word in enumerate(words):
+        print(word.rstrip(), end="")
+
+        if word.endswith((".", "?", ":")):
+            print("\n\n", end="")
+
+        else:
+            if i != len(words) - 1:
+                print(" ", end="")
+    """
+    lines = ".?:"
+
+    i = 0
+    while i < len(text) and text[i] == " ":
+        i += 1
+    while i < len(text):
+        print("{}".format(text[i]), end="")
+        if text[i] == "\n" or text[i] in lines:
+            if text[i] in lines:
+                print("{}".format("\n"))
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
+    """
