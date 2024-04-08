4970d463b1c23b446bb6592b702f98f8f2bf0219
nyaberi-mayaka <mayakanyaberi@gmail.com>
2024-04-08 04:16:46 -0400

    a function that multiplies 2 matrices by using the module NumPy with doctests

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiplies two matrices (m_a and m_b) using NumPy and returns the resulting product matrix.
+
+    Args:
+      m_a: A NumPy array representing the first matrix (m_a).
+      m_b: A NumPy array representing the second matrix (m_b).
+
+    Returns:
+      A NumPy array representing the product matrix of m_a and m_b.
+
+    Raises:
+      ValueError: If the inner dimensions of the matrices are not compatible for multiplication.
+  """
+
+    return (np.matmul(m_a, m_b))
