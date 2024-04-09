4544cd917bdeaa2fa15cb82f8c202e5c2cda3dfb
nyaberi-mayaka <mayakanyaberi@gmail.com>
2024-04-10 02:48:54 +0300

    a cass LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+a class LockedClass with no class or object attribute, that prevents the
+user from dynamically creating new instance attributes, except if the
+new instance attribute is called first_name.
+"""
+
+
 class LockedClass:
     """
     Prevents setting new instance attributes except for 'first_name'.
