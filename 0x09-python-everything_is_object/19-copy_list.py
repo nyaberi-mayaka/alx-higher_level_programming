1194a8ba8099b4389f43644f8792fb9e32a142e9
nyaberi-mayaka <mayakanyaberi@gmail.com>
2024-04-09 12:51:36 -0400

    a function that returns a copy of a list.

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
-"""a function that returns a copy of a list."""
-
 def copy_list(l):
-    """Returns a shallow copy of the input list.
-
-    Args:
-        l: The list to be copied.
-
-    Returns:
-        A new list containing a copy of the elements from the original list.
-
-    This function uses slicing to create a new list with the same elements
-    from the input list. Note that for nested mutable objects, this creates
-    shallow copies, meaning the nested objects themselves are still shared
-    references.
-    """
-
     return l[:]
