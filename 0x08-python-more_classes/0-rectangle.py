f2bbe51931691d97bf0263fb65eb88599451f0be
nyaberi-mayaka <mayakanyaberi@gmail.com>
2024-04-10 02:29:19 +0300

    rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,4 +2,11 @@
 """an empty class Rectangle that defines a rectangle"""
 
 class Rectangle():
+    """
+    An empty class named Rectangle representing a rectangle.
+
+    This class serves as a blueprint for creating rectangle objects,
+    but currently has no defined attributes or methods.
+    """
+
     pass
